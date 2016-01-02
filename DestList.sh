9ffc105c5ea49fd04920ed19e0582b22b05b5127
vahid <vh.khorasani@yahoo.com>
2016-01-02 19:48:26 +0330

    revision 5,list

--- DestList.sh
+++ DestList.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# These are for changing the color of texts
+# These are for changing the format of texts
 BlackRed="\033[31;40m"
 BlackGreen="\033[32;40m"
 BlackYellow="\033[33;40m"
 BlackBlue="\033[34;40m"
 BlackViolet="\033[35;40m"
 BlackCyan="\033[36;40m"
-BlackWhite="\033[37;40m"
 reset="\033[0m"
 
 # This is the destination to where the result will be saved
 file="${HOME}/destination.list"
+NUM_LINE=$(sort ${file} | wc -l)
+USERNAME=$(sort ${file} | cut -d ":" -f 3)
+NODE=$(sort ${file} | cut -d ":" -f 2)
+
 
 # This section is for creating a list from your most often used destinations
 echo -e "${BlackGreen}Please enter the hostname / IP address to add to your list:${reset} "
@@ -23,7 +26,8 @@ while [[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; do
   while [[ ${answer} = "y" ]]; do
 	read -p  "Hostname/IP: " dest
 	read -p "Enter a name to associate your destination: " name
-	echo ${dest}:${name} >> ${file}
+	read -p "Enter the username you use to login: " user
+	echo "'${name}'":${dest}:${user} >> ${file}
 	read -p "Is there any host to add to your list ? [y] " answer
 
 	  if	[[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; then
@@ -39,3 +43,18 @@ while [[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; do
   done
 
 done
+
+echo
+echo -e Â"${BlackGreen}Select your destionation:${reset} "
+
+#select destination in ${LIST}
+
+for (( i=1 ; i<=${NUM_LINE} ; i+=1 ))
+	do
+		echo -ne "${i}: $(sort ${file} | cut -d ":" -f 1 | sed -n ${i}p)\n"
+	done
+
+
+
+#		case $d in
+#			$(echo ${LIST} | sed -n '{i}p'))	echo  $(${USERNAME} | sed -n '${i}p')@$(${NODE} | sed -n '${i}p');;
