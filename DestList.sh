a1c653ac07ab127504a824bedc27620791c8a959
vahid <vh.khorasani@yahoo.com>
2016-01-02 09:17:38 +0330

    revision two(dest list)

--- DestList.sh
+++ DestList.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -x
-# text colors
+#!/bin/bash
+
+# These are for changing the color of texts
 BlackRed="\033[31;40m"
 BlackGreen="\033[32;40m"
 BlackYellow="\033[33;40m"
@@ -8,19 +9,24 @@ BlackViolet="\033[35;40m"
 BlackCyan="\033[36;40m"
 BlackWhite="\033[37;40m"
 reset="\033[0m"
+
+# This is the destination to where the result will be saved
 file="${HOME}/destination.list"
 
-echo -e "${BlackYellow}###Please enter the hostname / IP address to add to your list###${reset} "
+echo -e "${BlackGreen}Please enter the hostname / IP address to add to your list:${reset} "
+read -p "Is there any host you wanna add to your list ? [y] " answer
 
-read -p "Is there any host to add to your list ? [y] " answer
-
-while [[ ${answer} = "y" ]]; do
-if [[ -z ${answer} ]]; then
+while [[ -z ${answer} ]]; do
 	answer="y"
+
+	while [[ ${answer} = "y" ]]; do
 	read -p  "Hostname/IP: " dest
-elif [[ ${answer} == y ]]; then
-	read -p  "Hostname/IP: " dest
-elif [[ ${answer} != y ]]; then
-	echo -e "your list will be saved to ${HOME}/destinations.list "
-fi
+	echo ${dest} >> ${file}
+	read -p "Is there any host to add to your list ? [y] " answer
+	done
 done
+
+if [[ ${answer} != "y" ]]; then
+
+	echo -e "${BlackGreen}your list has been saved to${reset} ${BlackBlue}'${HOME}/destinations.list'${reset}"
+fi
