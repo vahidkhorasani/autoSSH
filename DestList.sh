b19696c7c522849dd2a9d746efeb49c758f5b3ed
vahid <vh.khorasani@yahoo.com>
2016-01-03 08:58:24 +0330

    revision 6,connect

--- DestList.sh
+++ DestList.sh
@@ -11,16 +11,16 @@ reset="\033[0m"
 
 # This is the destination to where the result will be saved
 file="${HOME}/destination.list"
-
+touch ${file}
 
 # This section is for creating a list from your most often used destinations
-echo -e "${BlackGreen}Please enter the hostname / IP address to add to your list:${reset} "
+echo -e "${BlackGreen}Make your own list${reset} "
 read -p "Is there any host you wanna add to your list ? [y] " answer
 
 while [[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; do
 	answer="y"
 
-  while [[ ${answer} = "y" ]]; do
+  	while [[ ${answer} = "y" ]]; do
 	read -p  "Hostname/IP: " dest
 	read -p "Enter a name to associate your destination: " name
 	read -p "Enter the username you use to login: " user
@@ -30,20 +30,19 @@ while [[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; do
 	  if	[[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; then
 	  	answer="y"
 	  elif	[[ ${answer} = "n" || ${answer} = "no" ]]; then
-		echo -e "your list has been saved to ${BlackGreen}'${HOME}/destinations.list'${reset}."
-		echo -e "you can check it out now:"
+		echo -e "your list has been saved to ${BlackGreen}'${HOME}/destinations.list'${reset}"
 		echo -ne "${BlackBlue}$(cat ${file})\n${reset}"
 	  else
 		echo -e "${BlackRed}NO VALID INPUT${reset}"
 		read -p "Is there any host to add to your list ? [y] " answer
 	  fi
-  done
+
+
+	done
 
 done
 
 NUM_LINE="$(sort ${file} | wc -l)"
-USERNAME="$(sort ${file} | cut -d ":" -f 3)"
-NODE="$(sort ${file} | cut -d ":" -f 2)"
 
 echo
 echo -e Â"${BlackGreen}Here are your most often used destionations:${reset} "
@@ -55,8 +54,18 @@ for (( i=1 ; i<=${NUM_LINE} ; i+=1 ))
 
 read -p "Enter your number to connect: " number
 
-echo -e "${BlackViolet}DEST:$(sort ${file} | sed -n ${number}p | cut -d ":" -f 1)${reset}"
-echo -e "${BlackViolet}IP/HOSTNAME:$(sort ${file} | sed -n ${number}p | cut -d ":" -f 2)${reset}"
-echo -e "${BlackViolet}Username:$(sort ${file} | sed -n ${number}p | cut -d ":" -f 3)${reset}"
+USERNAME="$(sort ${file} | sed -n ${number}p | cut -d ":" -f 3)"
+NODE_IP="$(sort ${file} | sed -n ${number}p | cut -d ":" -f 2)"
+NODE_NAME="$(sort ${file} | sed -n ${number}p | cut -d ":" -f 1)"
+
+echo -e "${BlackViolet}DEST:${NODE_NAME}${reset}"
+echo -e "${BlackViolet}IP/HOSTNAME:${NODE_IP}${reset}"
+echo -e "${BlackViolet}Username:${USERNAME}${reset}"
 
 read -p "Do you confirm ?[y]" confirm
+
+if [[ -z ${confirm} || ${confirm} = "y" || ${confirm} = "yes" ]]; then
+	ssh ${USERNAME}@${NODE_IP}
+else
+	exit 0
+fi
