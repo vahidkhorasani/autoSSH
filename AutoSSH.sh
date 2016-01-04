5d03ca2556328b90e19ce73358703d3136a0612f
vahid <vh.khorasani@yahoo.com>
2016-01-04 16:29:33 +0330

    guide edit

--- AutoSSH.sh
+++ AutoSSH.sh
@@ -21,7 +21,7 @@ while [[ -f ${file} ]]; do
 
 		cat <<- EOF
 		This is the first time you are running this script.
-		To get the most of it you need to make your own list including the most often used destinations.
+		To get the most of it you need to make your own list including your most often used destinations.
 		So, if you like to make it now, try "AutoSSH" with "-e" option.
 		Hope you enjoy it !
 		EOF
