163549876bce806acd33dec452fb798c632df9a4
vahid <vh.khorasani@yahoo.com>
2016-03-03 16:34:42 +0330

    Add feature, add destinations to '/etc/hosts'

--- AutoSSH.sh
+++ AutoSSH.sh
@@ -10,90 +10,91 @@ reset="\033[0m"
 
 # This is the destination to where the result will be saved
 file="${HOME}/destination.list"
+hosts="/tmp/hosts"
 backup="/tmp/destination.list.backup"
 touch ${file}
 touch ${backup}
+touch ${hosts}
 ######## This section is for creating a list from your most often used destinations
-
 # This part of script is for skipping the edition of your list if it exists.
 while [[ -f ${file} ]]; do
-
 	if [[ ! -s ${file} && $# -eq 0 ]]; then
-
 		cat <<- EOF
-		This is the first time you are running this script.
-		To get the most of it you need to make your own list including your most often used destinations.
+		You haven't any destination list to be used by 'AutoSSH'.
+		To get the most of this script you need to make your own list including your most often used destinations.
 		So, if you like to make it now, try "AutoSSH" with "-e" option.
-		Hope you enjoy it !
 		EOF
-
 		exit 0
 
-# This part is for edit flag
+# This part is for '-e' flag
 	elif [[ $# -eq 1 ]]; then
 
 while getopts :e option; do
 	case ${option} in
 		e)
 
-echo -e "${BlackGreen}Make your own list${reset} "
-read -p "Are you sure you wanna edit your list ? [y] " answer
+echo "Make your own list"
+read -p "Continue with editing your list ? [y] " answer
+read -p "Do you wanna add your destinations to your '/etc/hosts' file ? [y] " ETC
 
+if [[ -z ${ETC} || ${ETC} = "y" || ${ETC} = "yes" ]] && [[ ${USER} != root ]]; then
+	echo -e "${BlackRed}permission denied${reset}"
+	exit 0
+fi
 while [[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; do
 	answer="y"
 
   	while [[ ${answer} = "y" ]]; do
-	read -p  "Enter Hostname/IP: " dest
+	read -p "Enter Hostname/IP: " dest
 	read -p "Pick a name for this IP address: " name
 	read -p "Enter the username you wanna use to login: " user
 	echo "'${name}'":${dest}:${user} >> ${file}
+	echo "${dest}	${name}" >> ${hosts}
 	read -p "Is there any other hosts you wanna add to your list ? [y] " answer
 
 	  if	[[ -z ${answer} || ${answer} = "y" || ${answer} = "yes" ]]; then
 	  	answer="y"
 	  elif	[[ ${answer} = "n" || ${answer} = "no" ]]; then
-		echo -e "your list has been saved to ${BlackGreen}'${HOME}/destinations.list'${reset}"
-
-# Uncomment the following line if you wanna check the destination list with more detail.
+		echo "your list has been saved to '${file}'"
+		echo "you have also a backup list at '${backup}'"
+		# Uncomment the following line if you wanna check the destination list with more detail.
 		#echo -ne "${BlackBlue}$(cat ${file})\n${reset}"
 	  else
 		echo -e "${BlackRed}NO VALID INPUT${reset}"
 		read -p "Is there any other hosts you wanna add to your list ? [y] " answer
 	  fi
 
-
 	done
 
 done;;
-		?) echo -e "${BlackRed}I don't know what ${OPTARG} is!";;
+		?) echo -e "${BlackRed}I don't know what '${OPTARG}' is!${reset}";;
 	esac
+	#exit 0
 done
+
+if [[ -z ${ETC} || ${ETC} = "y" || ${ETC} = "yes" ]]; then
+	cat ${hosts} >> /etc/hosts
+fi
 
 fi
-		break
+	break
 done
-
 
 NUM_LINE="$(sort ${file} | wc -l)"
 
-echo
 rsync ${file} ${backup}
-echo -e "${BlackCyan}you have also a backup list at '${backup}'${reset}"
-echo -e "${BlackGreen}Here are your most often used destionations:${reset} "
+echo "Here are your most often used destionations: "
 
 for (( i=1 ; i<=${NUM_LINE} ; i+=1 ))
 	do
 		echo -ne "${i}: $(sort ${file} | cut -d ":" -f 1 | sed -n ${i}p)\n"
 	done
 
-# Script will be started from here if you run it without edit optin.
+# Script will be started from here if you run it without '-e' option
 read -p "Do you wanna connect now ? [y] " connect
 	while [[ -n ${connect} || ${connect} != "y" || ${connect} != "yes" || ${connect} != "n" || ${connect} != "no" ]]; do
-
 	if [[ -z ${connect} || ${connect} = "y" || ${connect} = "yes" ]]; then
-
 read -p "Enter a number to connect: " number
-
 while [[ ${number} -gt ${NUM_LINE} ]]; do
 	if [[ ${number} -gt ${NUM_LINE} ]]; then
 		echo -e "${BlackRed}your number is not in the valid range.${reset}"
